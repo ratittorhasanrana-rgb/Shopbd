edecc99cbcb87b5ec89732e6228477d39fd66485
ratittorhasanrana-rgb <ratittorhasanrana@gmail.com>
2026-02-26 14:01:58 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-  alert('মোবাইল মেনু খুলবে (পরে আরও সুন্দর করতে পারি)');
-});
-document.querySelectorAll('.add-to-cart').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert('✅ কার্টে যোগ হয়েছে! অর্ডার করতে WhatsApp করুন');
-  });
-});
